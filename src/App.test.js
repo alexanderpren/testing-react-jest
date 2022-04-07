f221130a9ff6b7be501465a41a4ac1a2dbbe821c
Alex <alexanderpren@gmail.com>
2022-04-07 16:46:45 -0500

    Button Color exercise testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('buttton has correct initial color and text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //find an element with a role of button and text of "Change to blue"
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  //expect the background color initially to be red
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+
+  //click button
+  fireEvent.click(colorButton);
+
+  //Expect th background color to be blue
+  expect(colorButton).toHaveStyle({backgroundColor: 'blue'})
+  //expect the text change to "Change to red"
+  expect(colorButton.textContent).toBe('Change to red');
+ 
 });
+
+
+
+
+
+
